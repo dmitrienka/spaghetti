6b40b8a0672cc080c8158be7f9b0c89b9fdc85f6
dmitrienka <dmitrienka@gmail.com>
2014-06-25 17:54:36 +0400

    * spaghetti.r: Новая функция поиска выбросов. Добавлена (кривая)   рисовалка по rtable

--- spaghetti.r
+++ spaghetti.r
@@ -1,23 +1,38 @@
 #!/usr/bin/Rscript
 options(warn=-1)
-Sys.setenv(LANG="EN")
+#Sys.setenv(LANG="EN")
 suppressMessages(library(data.table))
 suppressMessages(library(gdata))
 suppressMessages(library(ggplot2))
 suppressMessages(library(scales))
 suppressMessages(library(optparse))
 
+k.table <- c(5.62619251369954 ,3.59689767105988, 2.99683950381941, 2.9141936236358 , 
+3.37232041437195 ,2.93196313616901, 2.72771585462825, 2.70417682172506,
+2.86576896853064 ,2.66921102377684, 2.57092705221669, 2.55881443841962,
+2.64995441497466 ,2.53075618782533, 2.47791909362932, 2.4741828679544 ,
+2.53384546494551 ,2.45983379319381, 2.4212286638301 , 2.4159429171073 ,
+2.45784825894735 ,2.40879237590095, 2.38090443345185, 2.38216835038994,
+2.41048201093783 ,2.36989342570794, 2.35215022384351, 2.3524695182014 ,
+2.37772035649117 ,2.34643406722578, 2.33269558844597, 2.33379314746303,
+2.35213581186961 ,2.3293126888992 , 2.31585973838799, 2.32211834728897,
+2.33719246258054 ,2.31822219208683, 2.30836925652429, 2.30928647548234,
+2.32053355883361 ,2.30880999917026, 2.29983955811199, 2.30081635190135,
+2.31427557030017 ,2.29827012906655)
+
 
 
 option_list <- list(
-    make_option(c("-b", "--PdfWidth"),type="double", default=12,
-                help="Width of .pdf output file, [default %default]"),
-    make_option(c("-c", "--PdfHeight"), type="double",default=7,
-                help="Height of .pdf output file,, [default %default]"),
-    make_option(c("-d", "--EpsWidth"), type="double", default=6,
-                help="Width of .eps output file, [default %default]"),
-    make_option(c("-e", "--EpsHeight"), type="double", default=4,
-                help="Height of .eps output file, [default %default]"),
+    make_option(c("-W", "--width"),type="double", default=12,
+                help="Width of the output file, [default %default]"),
+    make_option(c("-H", "--height"), type="double",default=7,
+                help="Height of the output file, [default %default]"),
+    make_option(c("-p", "--Plot"), action="store_true", default=FALSE,
+                help="Use existing _rtable.dat for plotting, [default %default]"),
+    make_option(c("-e", "--Errors"), action="store_true", default=FALSE,
+                help="Use bond errors in outlier detection, [default %default]"),
+    make_option(c("-k", "--Kpar"), type="double",default=-1,
+                help="Use k in the outlier detection function, negatives for table values, [default %default]"),
     make_option(c("-a", "--Angles"), action="store_true", default=FALSE,
                 help="Use angles instead of bonds")			
     )
@@ -34,14 +49,21 @@ if (length(arguments$args) != 1)
 }
 
 
-
-
-
 directory <-   arguments$args
+
+
+if (opt$Kpar < 0){
+    kpar <- function(l) {if (l < 51) k.table[l-4] else k.table[46]}
+}else{
+    kpar <- function(l){opt$Kpar}
+}
+
+
+
 if (opt$Angles) {pattern <- "Angle_Restrain(_Breakable)?"
                  name.su <- "_angles"
              }else{
-                 pattern = "Distance_Restrain_(Breakable|Morse)"
+                 pattern = "Distance_Restrain(_Breakable|_Morse)?"
                  name.su <- "_bonds"}
 
 na.to.0 <- function(x) {if (is.na(x)) 0 else x}
@@ -69,23 +91,25 @@ extract.data <- function (filename) ###Индия!
             Bond[i] <- y[2]
         }
         qq <- quantile(Delta, c(0.25,0.75), names=FALSE)
-        Low.Q=qq[1]-1.5*diff(qq)
-        High.Q=qq[2]+1.5*diff(qq)
+        Low.Q=qq[1]-kpar(l.ys)*diff(qq)
+        High.Q=qq[2]+kpar(l.ys)*diff(qq)
         data.frame(Bond=Bond, Delta=Delta,
                    Error=Error, Av.Error=mean(Error),
                    Low.Q=Low.Q, High.Q=High.Q, 
-                   K1=k1, Rwp=rwp, Outlier = (Delta > (High.Q + 2 * Error)) |
-                                             (Delta < (Low.Q - 2 * Error)),
+                   K1=k1, Rwp=rwp, Outlier = (Delta > (High.Q + 2 * Error * opt$Errors)) |
+                                             (Delta < (Low.Q - 2 * Error * opt$Errors)),
                    Bad.Bond=FALSE)            
     }
 
 cat("Reading data..\n")
 
-topas.outs <- list.files(directory, full.names=TRUE, pattern="*.(out|OUT)")
-errors.table <- rbindlist(Map(extract.data, topas.outs))
+if (opt$Plot){
+    errors.table <- read.table(directory, row.names=FALSE, sep="\t")
+}else{
+    topas.outs <- list.files(directory, full.names=TRUE, pattern="*.(out|OUT)$")
+    errors.table <- rbindlist(Map(extract.data, topas.outs))}
 
 cat("Some calculations...\n")
-
 
 errors.table <- rbindlist(by(errors.table, errors.table$Bond, function(x) {x[["Bad.Bond"]] <-  any(x[["Outlier"]]); return(x)}))
 
@@ -93,8 +117,6 @@ errors.summary <- rbindlist(by(errors.table, errors.table$K1,
 	function(x){ data.frame(K1=x[["K1"]][1], Rwp=x[["Rwp"]][1],
 	RMS=round(sqrt(sum((x[["Delta"]])^2/length(x[["Delta"]]))), digits=4),
                                 Outliers=sum(x[["Outlier"]]))}))
-
-
 
 ## Plotting settings
 
@@ -105,13 +127,12 @@ PlotBreaks <- function(x){
 }
  
 p <- ggplot(data=errors.table, aes(x=K1, y=Delta)) + theme_bw()   # +  scale_x_continuous (breaks=PlotBreaks)
-if (sum(errors.table$Av.Error) >  0) {
+if ( opt$Errors &  sum(errors.table$Av.Error) >  0) {
 	p <- p +
 		geom_ribbon(aes(ymin=High.Q-2*Av.Error, ymax=High.Q+2*Av.Error, y=NULL), fill="#999999") +
 		geom_ribbon(aes(ymin=Low.Q-2*Av.Error, ymax=Low.Q+2*Av.Error, y=NULL), fill="#999999") 
 	}else {
-	cat("WARNING: You'd better use do_errors keyword in your *.inp files! \n")
-	p <- p +  geom_line(aes(y = High.Q), linetype="dashed") + geom_line(aes(y = Low.Q), linetype="dashed") }
+            p <- p +  geom_line(aes(y = High.Q), linetype="dashed") + geom_line(aes(y = Low.Q), linetype="dashed") }
 	
  if (sum(errors.table$Outlier) > 0){
      p <- p +
@@ -129,5 +150,6 @@ cat("Plotting and tables writing...\n")
 write.table(errors.table[order(errors.table$K1,errors.table$Bond),], paste(directory, name.su, "_rtable.dat", sep=""), row.names=FALSE, sep="\t")	
 write.fwf(errors.summary[order(errors.summary$K1)], paste(directory, name.su,"_summary.txt", sep=""))
 
-ggsave(p, file= paste(directory, name.su, "_plot.pdf", sep = ""), width=opt$PdfWidth, height=opt$PdfHeight)
-ggsave(p, file= paste(directory, name.su, "_plot.eps", sep = ""), width=opt$EpsWidth, height=opt$EpsHeight)
+ggsave(p, file= paste(directory, name.su, "_plot.pdf", sep = ""), width=opt$width, height=opt$height)
+ggsave(p, file= paste(directory, name.su, "_plot.eps", sep = ""), width=opt$width, height=opt$height)
+
